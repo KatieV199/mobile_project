5e6831ef0a1d32706288d29b9b33ff2fc5820a8f
KatieV199 <8939424@mail.ru>
2017-09-14 11:49:55 +0300

    add from 14.09

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-js
+
+
+	$('.multiple-items').slick({
+ 	 infinite: true,
+  	slidesToShow: 3,
+  	slidesToScroll: 3,
+  	prevArrow: '<button id="prev" type="button" class="arrow-slider prev"></button>',
+ 	 nextArrow: '<button id="next" type="button" class="arrow-slider next"></button>'
+	});
+
+
+	$('.track__list-slider-320').slick({
+ 	slidesToShow: 1,
+  	slidesToScroll: 1,
+ 	autoplay: true,
+  	autoplaySpeed: 2000,
+  	prevArrow: '<button id="prev" type="button" class="arrow-slider prev"></button>',
+  	nextArrow: '<button id="next" type="button" class="arrow-slider next"></button>'
+	});
+
+	$('.reviews__list-320').slick({
+	prevArrow: '<button id="prev" type="button" class="arrow-slider prev"></button>',
+  	nextArrow: '<button id="next" type="button" class="arrow-slider next"></button>'
+	});
+
+
+	$('.reviews__list').slick({
+  	slidesToShow: 3,
+  	slidesToScroll: 1,
+  	autoplay: true,
+ 	autoplaySpeed: 200000,
+  	prevArrow: '<button id="prev" type="button" class="arrow-slider prev"></button>',
+  	nextArrow: '<button id="next" type="button" class="arrow-slider next"></button>'
+	});
