36627bd95d4addf42e0ee27b6670d8210c9c7f7e
KatieV199 <8939424@mail.ru>
2017-09-15 17:21:19 +0300

    add from 15.09

--- js/script.js
+++ js/script.js
@@ -1,43 +1,51 @@
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: '<button id="prev" type="button" class="arrow-slider prev"></button>',
+    nextArrow: '<button id="next" type="button" class="arrow-slider next"></button>'
+});
 
 
-	$('.multiple-items').slick({
- 	 infinite: true,
-  	slidesToShow: 3,
-  	slidesToScroll: 3,
-  	prevArrow: '<button id="prev" type="button" class="arrow-slider prev"></button>',
- 	 nextArrow: '<button id="next" type="button" class="arrow-slider next"></button>'
-	});
+$('.track__list-slider-320').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<button id="prev" type="button" class="arrow-slider prev"></button>',
+    nextArrow: '<button id="next" type="button" class="arrow-slider next"></button>'
+});
 
 
-	$('.track__list-slider-320').slick({
- 	  slidesToShow: 1,
-  	slidesToScroll: 1,
- 	  autoplay: true,
-  	autoplaySpeed: 2000,
-  	prevArrow: '<button id="prev" type="button" class="arrow-slider prev"></button>',
-  	nextArrow: '<button id="next" type="button" class="arrow-slider next"></button>'
-	});
 
-	$('.reviews__list-320').slick({
-	prevArrow: '<button id="prev" type="button" class="arrow-slider prev"></button>',
-  	nextArrow: '<button id="next" type="button" class="arrow-slider next"></button>'
-	});
+$('.reviews__list').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 20000,
+    prevArrow: '<button id="prev" type="button" class="arrow-slider prev"></button>',
+    nextArrow: '<button id="next" type="button" class="arrow-slider next"></button>'
+});
+
+$('.reviews__list-320').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button id="prev" type="button" class="arrow-slider prev"></button>',
+    nextArrow: '<button id="next" type="button" class="arrow-slider next"></button>'
+});
 
 
-	$('.reviews__list').slick({
-  	slidesToShow: 3,
-  	slidesToScroll: 1,
-  	autoplay: true,
- 	autoplaySpeed: 200000,
-  	prevArrow: '<button id="prev" type="button" class="arrow-slider prev"></button>',
-  	nextArrow: '<button id="next" type="button" class="arrow-slider next"></button>'
-	});
+$(document).ready(function() {
+    $('.price-320__item').on('click', function() {
+        $(this).toggleClass('active-sl');
+        $('.price-320__item').not(this).removeClass('active-sl');
+    });
+});
 
-  $(document).ready(function() {
-  function accordion() {
-    $(this).toggleClass('active-sl');
-    $('.price-320__item').not(this).removeClass('active-sl');
-  }
-    $('.price-320__item').on('click', accordion);
-  
+$(document).ready(function() {
+    $('.page-header__toggler').on('click', function() {
+        $('.small-menu').toggleClass('active-t');
+
+        
+    });
 });
