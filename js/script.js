c11a557666adb2275ffd7bcce6854ec43f0602ab
KatieV199 <8939424@mail.ru>
2017-09-16 23:43:05 +0300

    dding from 16/09

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
 $('.multiple-items').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3,
-    prevArrow: '<button id="prev" type="button" class="arrow-slider prev"></button>',
-    nextArrow: '<button id="next" type="button" class="arrow-slider next"></button>'
-});
-
-
-$('.track__list-slider-320').slick({
-    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
@@ -17,9 +9,18 @@ $('.track__list-slider-320').slick({
 });
 
 
+$('.track__list-slider-320').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button id="prev" type="button" class="arrow-slider prev"></button>',
+    nextArrow: '<button id="next" type="button" class="arrow-slider next"></button>'
+});
+
+
+
 
 $('.reviews__list').slick({
-    slidesToShow: 3,
+    slidesToShow: $(document).width() > 320 ? 3 : 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 20000,
